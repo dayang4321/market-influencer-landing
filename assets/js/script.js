e38fb0cac04b064e83dc2ae4a50dec3847423515
dayang4321 <dayang4321@users.noreply.github.com>
2020-12-10 07:40:29 +0100

    implement scene3

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,19 +22,49 @@
 //   timeline.am('.pos1', { duration: 2, delay: 5, })
 
   //create a timeline instance
-var tl = gsap.timeline({duration: 1});
+var tl = gsap.timeline({
+ paused:true,
+  defaults: { duration: 3, ease: "power2.out" }
+});
 
 //the following two lines do the SAME thing:
 // tl.add( gsap.to("#id", {duration: 2, x: 100}) );
 tl.addLabel("scene1")
-    .to(".hero-section-one", { x: '54.65vw', width: '45.35vw', height: '100%' }, "scene1")
+  .to(".hero-section-one", { x: '54.65vw', width: '45.35vw', height: '100%' }, "scene1")
   .to(".hero-block", { marginTop: 0, height: '100%', backgroundColor: '#EFC111', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', }, "scene1")
-    .to(".hero-content-one", { opacity:0, display: 'none', position: 'absolute'}, "scene1")
-    .to(".hero-content-two", { opacity: 1, display: 'block', position: 'static', padding: '12.13vh 0 0 0',  }, "scene1")
-    .to(".hero-base", { yPercent: 100, onComplete: () => { $('.hero-base').hide() } }, "scene1")
-tl.pause()
+  .to(".hero-content-one", { opacity: 0, display: 'none', position: 'absolute' }, "scene1")
+  .to(".hero-content-two", { opacity: 1, display: 'block', position: 'static', padding: '12.13vh 0 0 0', }, "scene1")
+  .to(".hero-base", { yPercent: 100, onComplete: () => { $('.hero-base').hide() } }, "scene1")
+  .addLabel("scene2")
+  .to(".hero-section-one", { x: '0vw', width: '100vw', height: '34.8vh', position: 'absolute', bottom: 0 }, "scene2")
+  .to(".hero-block", { marginTop: 0, height: '100%', backgroundColor: '#17C7FF', boxShadow: 'none', }, "scene2")
+  .to(".hero-content-two", { opacity: 0, display: 'none', }, "scene2")
+  .to(".hero-content-three", { opacity: 1, display: 'block', position: 'static', }, "scene2")
+  .addLabel("scene3")
 
+  .addLabel("scene4");
+
+
+
+// var master = gsap.timeline(
+//   {paused:true}
+// );
 
 $('#firstResume').click(() => {
-  tl.resume()
+  //tl.tweenTo("scene2")
+  var tween1 = tl.tweenFromTo("scene1", "scene2");
+  //console.log(tween1)
+});
+
+$('#secondResume').click((event) => {
+  event.preventDefault()
+  var tween2 = tl.tweenFromTo("scene2", "scene3");
+  //console.log(tween2)
 })
+
+$('#thirdResume').click((event) => {
+  event.preventDefault()
+  var tween3 = tl.tweenFromTo("scene3", 0);
+  //console.log(tween2)
+})
+
