ac6a6155f928eee523c937598f646b6c0ff82236
dayang4321 <dayang4321@users.noreply.github.com>
2021-01-29 09:29:58 +0100

    add new form page

--- assets/js/transitions.js
+++ assets/js/transitions.js
@@ -22,20 +22,27 @@ gsap.registerPlugin(ScrollToPlugin);
      .to(".hero-content-three", { opacity: 0, }, "scene1")
      .to(".logo path:last-of-type", { fill: '#EFC111',}, "scene1")
      .addLabel("scene2")
-     .to(".hero-section-one", { x: '0vw', width: '100vw', height: '35%', position: 'absolute', top: 'unset', bottom: 0 }, "scene2")
-     .to(".hero-block", { marginTop: 0, height: '100%', backgroundColor: '#17C7FF', boxShadow: 'none', padding: '3.7vh 8.33vw' }, "scene2")
+     .to(".hero-section-one", { x: '65.677vw', width: '34.323vw', }, "scene2")
+     .to(".hero-block", { marginTop: 0, height: '100%', backgroundColor: '#61BB5A', boxShadow: 'none', padding: '0 6.8125rem' }, "scene2")
      .to(".hero-content-two", { opacity: 0, display: 'none', position:'absolute'}, "scene2")
      .to(".hero-content-three", {opacity: 1, display: 'block', position: 'static',}, "scene2")
      .to(window, {scrollTo: "max", onComplete:()=>{console.log('scrolled')}}, "scene2")
-     .to(".logo path:last-of-type", { fill:'#17C7FF',}, "scene2")
+     .to(".logo path:last-of-type", { fill:'#61BB5A',}, "scene2")
      .addLabel("scene3")
-     .to(".hero-section-one", { x: '0vw', width: '54.64vw', height: '37.7%', top: '26.5%'}, "scene3")
-     .to(".hero-block", {backgroundColor: '#F86624', padding: '5.2vh 8.33vw', width: '100%', height: '100%', position: 'relative'},"scene3")
-     .to(".hero-content-three", { opacity: 0, display: 'none', position: 'absolute', }, "scene3")
-     .to(".hero-content-four", { opacity: 1, display: 'block', position: 'static', }, "scene3")
-     .to(window, {scrollTo: {y: 100}, onComplete:()=>{console.log('scrolled')}}, "scene3")
-     .to(".logo path:last-of-type", { fill:'#F86624',}, "scene3")
-     .addLabel("scene4");
+     .to(".hero-section-one", { x: '0vw', width: '100vw', }, "scene3")
+     .to(".hero-block", { marginTop: 0, height: '100%', backgroundColor: '#1790FF', boxShadow: 'none', padding: '0 28.1875rem' }, "scene3")
+     .to(".hero-content-three", { opacity: 0, display: 'none', position:'absolute'}, "scene3")
+     .to(".hero-content-four", {opacity: 1, display: 'block', position: 'static',}, "scene3")
+     .to(window, {scrollTo: "max", onComplete:()=>{console.log('scrolled')}}, "scene3")
+     .to(".logo path:last-of-type", { fill:'#1790FF',}, "scene3")
+     .addLabel("scene4")
+     .to(".hero-section-one", { x: '0vw', width: '54.64vw', height: '37.7%', top: '26.5%'}, "scene4")
+     .to(".hero-block", {backgroundColor: '#F86624', padding: '5.2vh 8.33vw', width: '100%', height: '100%', position: 'relative'},"scene4")
+     .to(".hero-content-four", { opacity: 0, display: 'none', position: 'absolute', }, "scene4")
+     .to(".hero-content-five", { opacity: 1, display: 'block', position: 'static', }, "scene4")
+     .to(window, {scrollTo: {y: 100}, onComplete:()=>{console.log('scrolled')}}, "scene4")
+     .to(".logo path:last-of-type", { fill:'#F86624',}, "scene4")
+     .addLabel("scene5");
    
    
    
@@ -43,7 +50,7 @@ gsap.registerPlugin(ScrollToPlugin);
    //   {paused:true}
    // );
 
-   tl.tweenTo("scene2")
+   tl.tweenTo("scene4")
    
    $('#firstResume').click(() => {
      //tl.tweenTo("scene2")
